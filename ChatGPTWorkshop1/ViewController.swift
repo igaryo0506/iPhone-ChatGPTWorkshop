c5756a5cecd5db8a2f0d13561b76905562888260
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2024-05-10 19:58:06 +0900

    make ui

--- ChatGPTWorkshop1/ViewController.swift
+++ ChatGPTWorkshop1/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var resultLabels: [UILabel]!
+    private let resultsNum = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func search() {
+        
+    }
 }
 
